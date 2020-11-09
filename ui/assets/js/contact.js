3d035a8fcac89b655d8fe3304674de0267e40da1
olivierdesign <olivier.professional7@gmail.com>
2020-11-09 09:36:00 +0200

    Contact form to Firebase

--- ui/assets/js/contact.js
+++ ui/assets/js/contact.js
@@ -1,68 +1,50 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC6w5-nZISQZLZTDYzQfVWRdr31NOFMpks",
+    authDomain: "portfolio-2c210.firebaseapp.com",
+    databaseURL: "https://portfolio-2c210.firebaseio.com",
+    projectId: "portfolio-2c210",
+    storageBucket: "portfolio-2c210.appspot.com",
+    messagingSenderId: "730713652270",
+    appId: "1:730713652270:web:e1ff2507783eaf59b973bf",
+    measurementId: "G-Y9VZQME0B6"
+  };
 
-//   console.log(firebase);
-//Reference messages collection
-// var messagesRef = firebase.database().ref('message');
-
-//Listen for form submit
-// document.getElementById('contact-form').addEventListener('submit', submitForm);
-// document.querySelector('.contact-form').addEventListener('submit',submitForm);
-//Submit form
-// function submitForm(e){
-//     e.preventDefault();
-//     console.log('123');
-//Get values
-//     var name = getInputval('name');
-//     var email = getInputval('email');
-//     var telephone = getInputval('Telephone');
-//     var message = getInputval('message');
-// }
-
-//Save message
-// saveMessage(name, email, telephone, message);
-
-//function to get get form values
-// function getInputval(id){
-//     return document.getElementById(id).value;
-// }
-// console.log('hhhhh');
-//Save message to firebase
-// function saveMessage(name, email, telephone,message){
-//     db.collection("message").doc().set({
-//         'name': name,
-//         'email': email,
-//         'telephone': telephone,
-//         'message': message
-//     })
-//     .then(function() {
-//         console.log("Document successfully written!");
-//     })
-//     .catch(function(error) {
-//         console.error("Error writing document: ", error);
-//     })
-// }
-
-
-var messagesdb=firebase.database().ref("message");
-
-document.getElementById('contact-form').addEventListener('submit', submitForm);
-function submitForm(e) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+  // Reference contactInfo collections
+  var contactInfo = firebase.database().ref("infos");
+  
+  // Listen for a submit
+  document.querySelector('.contactForm').addEventListener('submit', submitForm);
+  // document.getElementById('contact-form').addEventListener('submit', submitForm);
+  function submitForm(e) {
     e.preventDefault();
-    var fullname=document.getElementById('fname').value;
-    var email=document.getElementById('email').value;
-    var telephone=document.getElementById('telephone').value;
-    var message=document.getElementById('message').value;
-addmessage(fullname, email, telephone, message);
-document.getElementById('contact-form').reset();
-}
-function addmessage (fullname, email, telephone, message) {
-  var newMessagedb = messagesdb.push();
-  newMessagedb.set({
-    name: fullname,
-    email: email,
-    telephone: telephone,
-    message: message
-  })
-    }
-// function myInput(id) {
-//     return document.getElementById(id).value;
-// }
+  
+    //   Get input Values
+    var name = document.querySelector(".name").value;
+    var email = document.querySelector(".email").value;
+    var telephone = document.querySelector(".telephone").value;
+    var message = document.querySelector(".message").value;
+    console.log(name, email, telephone, message);
+  
+    saveContactInfo(name, email, telephone, message);
+  
+    document.querySelector(".contactForm").reset();
+    var send = 
+  }
+  
+  // Save infos to Firebase
+  function saveContactInfo(name, email, telephone, message) {
+    var newContactInfo = contactInfo.push();
+  
+    newContactInfo.set({
+      name: name,
+      email: email,
+      telephone: telephone,
+      message: message,
+    });
+  }
